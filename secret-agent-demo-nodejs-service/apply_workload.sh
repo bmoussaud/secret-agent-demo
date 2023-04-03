821455cb2bf102ee0f46ba3fa307962519523291
Benoit Moussaud <benoit@moussaud.org>
2023-04-03 19:06:05 +0200

    update devoxx

--- secret-agent-demo-nodejs-service/apply_workload.sh
+++ secret-agent-demo-nodejs-service/apply_workload.sh
@@ -1,2 +1,2 @@
-NS=kcd-fr-23
-tanzu apps workload apply -f config/workload.yaml  --live-update --local-path . --source-image akseutap4registry.azurecr.io/sb-nodejs-demo --namespace $NS --yes  --update-strategy merge
+NS=secret-agent
+tanzu apps workload apply -f config/workload.yaml  --live-update --local-path . --source-image akseutap5registry.azurecr.io/sb-nodejs-demo-src --namespace $NS --yes  --update-strategy merge
